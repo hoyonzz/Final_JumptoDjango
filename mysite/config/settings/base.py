f7d58a4706aa5439478d6346205f9fa84f8757e9
hoyong.shin <hoyong6737@gmail.com>
2025-09-15 20:25:27 +0900

    settings 분리 설정

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g1em=gs0roclllf*p04vvzwfmr_q13=lkw_d=6=@z^kt30ud@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['13.209.106.188']
+ALLOWED_HOSTS = []
 
 
 # Application definition
---
+++ mysite/config/settings/prod.py
@@ -0,0 +1,6 @@
+from .base import *
+
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['13.209.106.188']
