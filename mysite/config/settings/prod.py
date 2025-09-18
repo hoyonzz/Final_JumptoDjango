7c4f620d310e34b58959ed02e580160658acb19d
hoyong.shin <hoyong6737@gmail.com>
2025-09-18 19:38:49 +0900

    데이터베이스 적용

--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -6,3 +6,14 @@ DEBUG = False
 ALLOWED_HOSTS = ['13.209.106.188']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'DJANGO.DB.BACKENDS.POSTGRESQL_PSYCOPG2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'kkNY[:f8pZ%bFU(d]2!2]s7Gb>{AB#4g'
+        'HOST': 'ls-08b690b662e41c3e713828df17c91339294fc316.cxgeogskomfe.ap-northeast-2.rds.amazonaws.com'
+        'PORT': '5432',
+    }
+}
